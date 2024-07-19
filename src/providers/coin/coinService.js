0b8cfc73f63b706697f58e907965bfd1a74d654b
nikhilyadav111 <ny688634@gmail.com>
2024-07-19 14:57:28 +0530

    jhghjg

--- src/providers/coin/coinService.js
+++ src/providers/coin/coinService.js
@@ -3,6 +3,7 @@ import axios from "axios";
 const fetchTrending = async () => {
   const response = await axios.get(
     "https://api.coingecko.com/api/v3/search/trending"
+ 
   );
 
   return response.data.coins;
@@ -11,6 +12,7 @@ const fetchTrending = async () => {
 const fetchCoin = async (id) => {
   const response = await axios.get(
     `https://api.coingecko.com/api/v3/coins/${id}`
+    
   );
 
   return response.data;
